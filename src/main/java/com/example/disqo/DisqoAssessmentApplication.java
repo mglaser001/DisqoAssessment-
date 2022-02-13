c6e9cd932e6873ceeedd29d7429f664fd1b8ef68
mglaser001 <mglaser001@gmail.com>
2022-02-12 23:31:30 -0800

    Initial commit

---
+++ src/main/java/com/example/disqo/DisqoAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.disqo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisqoAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisqoAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/disqo/DisqoAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disqo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisqoAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
